77ee4337655baa3aec6b4f5afc019a51b786a6ff
이종현 <dlwhdgus53@naver.com>
2021-03-12 18:09:59 +0900

    비밀번호 설정 및 아이디찾기

--- WebContent/jsMain/Main.js
+++ WebContent/jsMain/Main.js
@@ -60,6 +60,13 @@
 //  			//사용자의 이름을 가져오고 작성 제목,작성 내용을 넘긴다. 이후에 DB에 저장
 //  		});
   		
+  		// 등록 버튼 누를 시 회원가입
+  		$('#enroll-btn').click(function() {
+  			alert('회원가입되었습니다.')
+  		})
+  		
+  		
+  		//
   		$('#btn-search').on('click', function() {
   			
   			window.open('http://localhost:8080/0312_pair_project/searchResult.html');
@@ -67,6 +74,78 @@
 
   		});
   		
+  		// 비밀번호 길이 체크
+		$('#pwd').keyup(function() {
+//			if($(this).val()=='' || $(this).val().length==0){
+//				$('#pw-msg').text('패스워드를 입력하세요');
+//				$('#pw-msg').css('color', 'red');
+//			}else if($(this).val().length<4){
+//				$('#pw-msg').text('안전도 낮음');
+//				$('#pw-msg').css('color', 'red');
+//			}else{
+//				$('#pw-msg').text('안전도 높음');
+//				$('#pw-msg').css('color', 'green');
+//			}
+			
+			
+			
+			 var pw = $("#pwd").val();
+			 var num = pw.search(/[0-9]/g);
+			 var eng = pw.search(/[a-z]/ig);
+			 var spe = pw.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+			 if(pw.length < 8 || pw.length > 20){
+				 $('#pw-msg').text('길이가 너무 길거나 짧습니다.');
+				 $('#pw-msg').css('color', 'red');
+				 return false;
+			 }else if(pw.search(/\s/) != -1){
+				 $('#pw-msg').text("비밀번호는 공백 없이 입력해주세요.");
+				 $('#pw-msg').css('color', 'blue');
+				 return false;
+			 }else if(num < 0 || eng < 0 || spe < 0 ){
+				 $('#pw-msg').text("영문,숫자, 특수문자를 혼합하여 입력해주세요.");
+				 $('#pw-msg').css('color', 'blue');
+				 return false;
+			 }else {
+				 $('#pw-msg').text("사용가능합니다."); 
+				 $('#pw-msg').css('color', 'green');
+			    return true;
+			 }
+			
+			
+			
+			
+		})
+		
+
+		//비밀번호 체크
+		$('#btn-find-pwd').click(function() {
+			
+			$.ajax({
+				url :'jsMain/userlist.jsp',
+				type : 'GET',
+				dataType : 'xml',
+				success: function(response){
+					checkPwd(response);
+				}	
+			});
+		});
+  
+		function checkPwd(response){
+			let id = $('#pwd-find').val();
+			
+  			$(response).find('user').each(function(index,item){
+  				var dbId = $(this).find('id').text();
+  				var dbPwd = $(this).find('password').text();
+  				if(id==dbId){
+  					alert('비밀번호는'+dbPwd+'입니다.');
+  					return false;
+  				}
+  				//존재하지않으면 아예 존재하지 창이 아예 닫힌다.
+  			});
+			
+			
+		}
   		
   		
   		
